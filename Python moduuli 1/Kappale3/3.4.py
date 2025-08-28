d6a12d126960dd555da0052f0a768220d1987c6f
Jonttu1000 <jonatangomesma@gmail.com>
2025-08-28 09:46:16 +0300

    update

--- Python moduuli 1/Kappale3/3.4.py
+++ Python moduuli 1/Kappale3/3.4.py
@@ -1,9 +1,7 @@
 vuosi = int(input("Anna vuosiluku"))
-chekki1 = vuosi/4
-chekki2 = vuosi/100
-chekki3 = vuosi/400
+chekki = vuosi/4
 
-if chekki1 != int(chekki1) or chekki2 != int(chekki2) and chekki3 != int(chekki3):
+if chekki != int(chekki):
     print(f"Vuosi {vuosi} ei ole karkaus vuosi ")
 else:
     print(f"Vuosi {vuosi} on karkaus vuosi")

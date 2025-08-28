fffc7a847f7db32397211f1640526a4c4e28569f
Jonttu1000 <jonatangomesma@gmail.com>
2025-08-28 18:51:02 +0300

    update

--- Python moduuli 1/Kappale4/4.4.py
+++ Python moduuli 1/Kappale4/4.4.py
@@ -1,13 +1,14 @@
 import random
 arvoitus= (random.randint(1,10))
 oikein=1
+pelaajan_arvaus = input("Arvaa lukuni väliltä 1-10")
 while oikein==1:
-    pelaajan_arvaus = input("Arvaa lukuni väliltä 1-10")
+
     arvoitus2 = int(arvoitus)
     if int(pelaajan_arvaus) < int(arvoitus2):
-        print("Lukuni on suurempi")
+        pelaajan_arvaus = input("Lukuni on suurempi")
     elif int(pelaajan_arvaus) > int(arvoitus2):
-        print("Lukuni on pienempi")
+        pelaajan_arvaus = input("Lukuni on pienempi")
     elif int(pelaajan_arvaus) == arvoitus2:
         print("Oikein!")
         oikein=0
